cf18c0b58674fcf83d43899e9718cb033848412a
안혜연 <ahy020606@naver.com>
2026-06-18 21:46:32 +0900

    etc: pxr 주석 추가

--- src/common/utils/pxr.ts
+++ src/common/utils/pxr.ts
@@ -1,3 +1,5 @@
+// inline style에서 px 값을 rem으로 변환할 때 사용. Tailwind 클래스로 표현 불가능한 동적 값에만 사용할 것.
+// 예: <div style={{ height: pxr(headerHeight) }}>
 const pxr = (px: number) => `${px / 16}rem`
 
 export default pxr
