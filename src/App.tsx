08bd6be18571d868d714767c2436c8855c0816d5
kminnaaa <bexemen@gmail.com>
2026-06-21 11:37:36 +0900

    feat: paycheckPlan 라우트 등록

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ import MypagePage from './mypage/MypagePage'
 import NotificationPage from './notification/NotificationPage'
 import NotificationSettingsPage from './notification/NotificationSettingsPage'
 import SurveyPage from './survey/SurveyPage'
+import PaycheckAssetSelectPage from './paycheckPlan/PaycheckAssetSelectPage'
+import PaycheckDiagnosisPage from './paycheckPlan/PaycheckDiagnosisPage'
+import PaycheckPlansPage from './paycheckPlan/PaycheckPlansPage'
+import PaycheckComparePage from './paycheckPlan/PaycheckComparePage'
+import PaycheckPlanDetailPage from './paycheckPlan/PaycheckPlanDetailPage'
+import PaycheckExecutePage from './paycheckPlan/PaycheckExecutePage'
+import PaycheckConsultPage from './paycheckPlan/PaycheckConsultPage'
 
 const router = createBrowserRouter([
   { path: '/', element: <SplashPage /> },
@@ -15,6 +22,13 @@ const router = createBrowserRouter([
   { path: '/mypage', element: <MypagePage /> },
   { path: '/notification', element: <NotificationPage /> },
   { path: '/notification/settings', element: <NotificationSettingsPage /> },
+  { path: '/paycheck-plan/assets', element: <PaycheckAssetSelectPage /> },
+  { path: '/paycheck-plan/diagnosis', element: <PaycheckDiagnosisPage /> },
+  { path: '/paycheck-plan/plans', element: <PaycheckPlansPage /> },
+  { path: '/paycheck-plan/compare', element: <PaycheckComparePage /> },
+  { path: '/paycheck-plan/plans/:planId', element: <PaycheckPlanDetailPage /> },
+  { path: '/paycheck-plan/execute', element: <PaycheckExecutePage /> },
+  { path: '/paycheck-plan/consult', element: <PaycheckConsultPage /> },
 ])
 
 function App() {
