b46f031bf954b8779b286e54249352130fc0e3a6
kim junsoo <kjs201105@naver.com>
2026-06-25 09:14:14 +0900

    refactor: 온보딩 플로우를 단계별 라우트로 분리
    - OnboardingPage를 OnboardingContextProvider + Outlet 레이아웃으로 변경
    - OnboardingRoutes에 각 단계별 라우트 래퍼 컴포넌트 추가
    - App.tsx를 /onboarding 중첩 라우트 구조로 변경
    - pensionStatus 조건 분기(step4, asset-intro)를 라우트 래퍼에서 처리

--- src/onboarding/OnboardingPage.tsx
+++ src/onboarding/OnboardingPage.tsx
@@ -1,60 +1,10 @@
-// src/onboarding/OnboardingPage.tsx
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { OnboardingContextProvider } from './contexts/OnboardingContext'
-import { useStepNavigation } from './hooks/useStepNavigation'
-import TermsAgree from './components/TermsAgree'
-import Step1Situation from './components/steps/Step1Situation'
-import Step2Pension from './components/steps/Step2Pension'
-import Step3BasicInfo from './components/steps/Step3BasicInfo'
-import Step4Living from './components/steps/Step4Living'
-import Step5PensionDefer from './components/steps/Step5PensionDefer'
-import AssetIntro from './components/asset/AssetIntro'
-import AssetAuth from './components/asset/AssetAuth'
-import AssetConsent from './components/asset/AssetConsent'
-import AssetLoading from './components/asset/AssetLoading'
-import AssetResult from './components/asset/AssetResult'
-
-function OnboardingContent() {
-  const [currentStep, setCurrentStep] = useState(0)
-  const navigate = useNavigate()
-  const { getNextStep, getPrevStep } = useStepNavigation()
-
-  function handleNext() {
-    setCurrentStep(s => getNextStep(s))
-  }
-
-  function handlePrev() {
-    if (currentStep === 0) {
-      navigate(-1)
-      return
-    }
-    setCurrentStep(s => getPrevStep(s))
-  }
-
-  const stepProps = { onNext: handleNext, onPrev: handlePrev }
-
-  return (
-    <>
-      {currentStep === 0 && <TermsAgree {...stepProps} />}
-      {currentStep === 1 && <Step1Situation {...stepProps} />}
-      {currentStep === 2 && <Step2Pension {...stepProps} />}
-      {currentStep === 3 && <Step3BasicInfo {...stepProps} />}
-      {currentStep === 4 && <Step4Living {...stepProps} />}
-      {currentStep === 5 && <Step5PensionDefer {...stepProps} />}
-      {currentStep === 6 && <AssetIntro onNext={handleNext} onPrev={handlePrev} />}
-      {currentStep === 7 && <AssetAuth {...stepProps} />}
-      {currentStep === 8 && <AssetConsent {...stepProps} />}
-      {currentStep === 9 && <AssetLoading onNext={handleNext} />}
-      {currentStep === 10 && <AssetResult />}
-    </>
-  )
-}
 
 function OnboardingPage() {
   return (
     <OnboardingContextProvider>
-      <OnboardingContent />
+      <Outlet />
     </OnboardingContextProvider>
   )
 }
